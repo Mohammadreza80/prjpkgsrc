d64186d2104312afd584609ba490c954c37a7d40
reza <reza.sorori@gmail.com>
2024-04-11 16:44:16 +0800

    adding test

--- mytest.py
+++ mytest.py
@@ -1,4 +1,15 @@
 from timmins.foo import demo
+from timmins import foo
+from timmins.foo import module1
+from timmins import module_t
+from timmins.module_t import Modulet
 
 demo.m1()
+foo.demo.m1()
+obj = module1.Module1()
+obj.say_hi()
+obj2 = module_t.Modulet()
+obj2.say_hi_t()
+obj3 = Modulet()
+obj3.say_hi_t()
 # sdf
---
+++ src/timmins/module_t.py
@@ -0,0 +1,3 @@
+class Modulet:
+    def say_hi_t(self):
+        print("hi from module_t")
